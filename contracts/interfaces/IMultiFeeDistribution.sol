0826e1e8c13eab4b08c71a9750ce8cb535cbad36
chainer2021 <chainer2021@163.com>
2022-05-06 17:02:31 +0800

    add loft incentives support

---
+++ contracts/interfaces/IMultiFeeDistribution.sol
@@ -0,0 +1,11 @@
+pragma solidity 0.7.6;
+
+interface IMultiFeeDistribution {
+  function addReward(address rewardsToken) external;
+
+  function mint(
+    address user,
+    uint256 amount,
+    bool withPenalty
+  ) external;
+}
